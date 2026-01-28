53455e76f76780f86fd252623808e363000ce68c
Tejas G N <116544918+tejasgn2002@users.noreply.github.com>
2026-01-28 05:16:10 +0000

    Delete Shopping-Cart directory

--- Shopping-Cart/src/main/java/com/ecom/app/exceptions/CartAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.ecom.app.exceptions;
-
-public class CartAlreadyExistsException extends RuntimeException{
-    public CartAlreadyExistsException(String message){
-        super(message);
-    }
-}
--- Shopping-Cart/src/main/java/com/ecom/app/exceptions/UsernameAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.ecom.app.exceptions;
-
-public class UsernameAlreadyExistsException extends RuntimeException{
-    public UsernameAlreadyExistsException(String message){
-        super(message);
-    }
-}
