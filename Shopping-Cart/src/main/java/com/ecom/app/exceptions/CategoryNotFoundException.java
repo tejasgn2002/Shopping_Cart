1eb845246fb946ff52953f99b03abecb8d1a393b
Tejas G N <116544918+tejasgn2002@users.noreply.github.com>
2026-02-05 07:16:55 +0000

    Delete Shopping-Cart/src directory

--- Shopping-Cart/src/main/java/com/ecom/app/exceptions/CategoryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ecom.app.exceptions;
-
-public class CategoryNotFoundException extends RuntimeException{
-    public CategoryNotFoundException(String message){
-        super(message);
-    }
-}
--- Shopping-Cart/src/main/java/com/ecom/app/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ecom.app.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message){
-        super(message);
-    }
-}
